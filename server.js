159dc417efb2453efeafea4cfe3148a300c5e079
altRush <rushing.wp@gmail.com>
2024-07-17 17:08:07 +0700

    feat: Make server send message in binary mode

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ import { WebSocketServer } from 'ws';
 const wss = new WebSocketServer({ port: 8080 });
 
 wss.on('connection', ws => {
-	ws.on('message', message => {
-		console.log('Received: ', message);
+	ws.on('message', (message, isBinary) => {
+		wss.clients.forEach(client => {
+			if (client !== ws && client.readyState === WebSocket.OPEN) {
+				client.send(message, { binary: isBinary });
+			}
+		});
 	});
-
-	ws.send('Heya!');
 });
