30ff2f6e9747890fa0b1c010b67377d88e82038d
altRush <rushing.wp@gmail.com>
2024-07-18 12:08:29 +0700

    feat: Basic chat completed

--- server.js
+++ server.js
@@ -4,8 +4,9 @@ const wss = new WebSocketServer({ port: 8080 });
 
 wss.on('connection', ws => {
 	ws.on('message', (message, isBinary) => {
+		console.log('%s', message);
 		wss.clients.forEach(client => {
-			if (client !== ws && client.readyState === WebSocket.OPEN) {
+			if (client.readyState === WebSocket.OPEN) {
 				client.send(message, { binary: isBinary });
 			}
 		});
--- src/features/chat/index.tsx
+++ src/features/chat/index.tsx
@@ -1,6 +1,7 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import './index.css';
-import useWebSocket, { ReadyState } from 'react-use-websocket';
+import useWebSocket from 'react-use-websocket';
+import { WebSocketMessage } from 'react-use-websocket/dist/lib/types';
 
 const WS_URL = 'ws://localhost:8080';
 
@@ -8,11 +9,23 @@ const ChatBox = () => {
 	// eslint-disable-next-line @typescript-eslint/no-explicit-any
 	const [messageHistory, setMessageHistory] = useState<MessageEvent<any>[]>([]);
 
-	const { sendMessage, lastMessage, readyState } = useWebSocket(WS_URL, {
+	const [chatText, setChatText] = useState('');
+	const [sender, setSender] = useState('');
+
+	const { sendMessage, lastMessage } = useWebSocket(WS_URL, {
 		onOpen: () => {
 			console.log('Opened !');
 		}
 	});
+
+	const handleClickSendMessage = useCallback(
+		(message: WebSocketMessage) => sendMessage(message),
+		[sendMessage]
+	);
+
+	const isSender = (incomingSender: string): boolean => {
+		return sender === incomingSender;
+	};
 
 	useEffect(() => {
 		if (lastMessage !== null) {
@@ -20,31 +33,63 @@ const ChatBox = () => {
 		}
 	}, [lastMessage]);
 
-	// const handleClickSendMessage = useCallback(() => sendMessage('Hello'), []);
-
-	// const connectionStatus = {
-	// 	[ReadyState.CONNECTING]: 'Connecting',
-	// 	[ReadyState.OPEN]: 'Open',
-	// 	[ReadyState.CLOSING]: 'Closing',
-	// 	[ReadyState.CLOSED]: 'Closed',
-	// 	[ReadyState.UNINSTANTIATED]: 'Uninstantiated'
-	// }[readyState];
-
 	return (
 		<div className="chatbox-container">
 			<div>
-				<img className="chat-logo" src={'/chat-room-svgrepo-com.svg'} alt="" />
+				<img className="chat-logo" src={'/chat-room.svg'} alt="" />
 				<h3>
 					Just <span className="talk-logo">talk</span>™
 				</h3>
+				<div>
+					Name:
+					<input
+						onChange={e => {
+							setSender(e.target.value);
+						}}
+						type="text"
+					/>
+				</div>
 			</div>
 			<div className="chat-box">
 				<div className="chat-box-title">Chat</div>
 				<div className="chat-box-body">
 					{messageHistory.map((message, idx) => {
-						console.log(message.data);
-						return <div key={idx}>{message ? message.data : null}</div>;
+						const { sender, messageText } = JSON.parse(message.data);
+						const ownMessage = isSender(sender);
+						return (
+							<div className={ownMessage ? 'sender-text' : ''} key={idx}>
+								{messageText
+									? `${!ownMessage ? `[${sender}] ` : ''}${messageText}`
+									: null}
+							</div>
+						);
 					})}
+				</div>
+				<div className="chat-box-input">
+					<div>
+						<input
+							value={chatText}
+							onChange={e => setChatText(e.target.value)}
+							placeholder="..what's on your mind?"
+							type="text"
+						/>
+					</div>
+					<div>
+						<img
+							className="send-message-icon"
+							src={'/send-message.svg'}
+							alt=""
+							onClick={() => {
+								handleClickSendMessage(
+									JSON.stringify({
+										sender,
+										messageText: chatText
+									})
+								);
+								setChatText('');
+							}}
+						/>
+					</div>
 				</div>
 			</div>
 		</div>
